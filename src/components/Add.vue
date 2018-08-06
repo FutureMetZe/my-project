905c3e288a119bfe948a6f51ed54255850f795be
future <keqimi@qq.com>
2018-08-06 10:17:16 +0800

    Initial commit

---
+++ src/components/Add.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="col-md-4">
+    <form class="form-horizontal">
+      <div class="form-group">
+        <label>用户名</label>
+        <input type="text" class="form-control" placeholder="用户名" v-model="name">
+      </div>
+      <div class="form-group">
+        <label>评论内容</label>
+        <textarea class="form-control" rows="6" placeholder="评论内容" v-model="content"></textarea>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button @click="add" type="button" class="btn btn-default pull-right">提交</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Add",
+      props:{
+        addComment:{ //指定属性名，属性值的类型，必要性
+          type: Function,
+          require:true
+        }
+      },
+      data(){
+          return {
+            name:'',
+            content:''
+          }
+      },
+      methods:{
+        add(){
+          //1.合法
+          const name = this.name.trim();
+          const content = this.content.trim();
+          if(!name||!content){
+            alert("不能为空1")
+          }
+          //2.输入的数据分装成对象
+          const  comment={
+            name,content
+          }
+          //3.添加到comments中
+          this.addComment(comment);
+          //4.清除输入
+          this.name = this.content = '';
+
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <div class="col-md-8">
+    <h3 class="reply">评论回复：</h3>
+    <h2 v-show="comments.length==0">暂无评论，点击左侧添加评论！！！</h2>
+    <ul class="list-group">
+      <Item v-for="(comment, index) in comments" :key="index" :comment="comment"
+      :deleteComment="deleteComment" :index="index"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+    import Item from "./Item";
+    export default {
+        name: "List",
+      components: {Item},
+      /*声明接受属性才可以接收到父组件传过来的内容
+      * 这个属性就会成为组件对象的属性*/
+      props:{
+        comments:Array,
+        deleteComment:Function
+      }, //只指定属性名
+      data(){
+        return{
+          a:1
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .reply {
+    margin-top: 0px;
+  }
+
+
+</style>
